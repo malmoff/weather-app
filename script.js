dca955aa45ef8cc5c1ae0f039b026db2272a7a04
malmoff <81243022+malmoff@users.noreply.github.com>
2023-07-25 12:54:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,15 +21,15 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".city").innerText = "Vädret i " + name;
     document.querySelector(".icon").src =
       "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
+    document.querySelector(".description").innerText = beskrivning;
     document.querySelector(".temp").innerText = temp + "°C";
     document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
+      "Luftfuktighet: " + humidity + "%";
     document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
+      "Vindhastighet: " + speed + " km/h";
     document.querySelector(".weather").classList.remove("loading");
     document.body.style.backgroundImage =
       "url('https://source.unsplash.com/1600x900/?" + name + "')";
